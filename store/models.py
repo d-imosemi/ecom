ddd71a4d60fd1c9feabd5d1cd04da3d9743af0ad
d-imosemi <83042447+d-imosemi@users.noreply.github.com>
2022-05-11 15:26:06 +0100

    Initial commit

---
+++ store/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+#cutomer details
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
+    phonenumber = models.CharField(max_length=11, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+#product details
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    #funtion to render image properly when image is not available
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = "static/images/placeholder.png"
+        return url
+
+#order details (cart)
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_orderd = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+
+
+    #function for shipping only non-digital
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
+
+    #a function to get total cart item and total cart price
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+    def __str__(self):
+        return str(self.id)
+
+#order item within cart
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    #to get the total price in cart
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+    
+
+    def __str__(self):
+        return str(self.product)
+
+
+#shipping model
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
